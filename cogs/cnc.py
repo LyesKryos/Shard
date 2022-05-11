421017055ebb0050336867fe70dce37dec9b3474
LyesKryos <vizior.slosky@gmail.com>
2022-05-11 19:22:07 -0400

    CNC loop testing

--- cogs/cnc.py
+++ cogs/cnc.py
@@ -3405,7 +3405,7 @@ class CNC(commands.Cog):
             await shardchannel.send(f"Waiting until {update.strftime('%d %a %Y at %H:%M:%S %Z%z')}.")
             await discord.utils.sleep_until(update)
         elif now.time() > datetime.time(hour=19, minute=0):
-            update = now.replace(hour=19, minute=22, second=0)
+            update = now.replace(hour=19, minute=22, second=30)
             # update += datetime.timedelta(days=1)
             await shardchannel.send(f"CnC loop waiting until {update.strftime('%d %a %Y at %H:%M:%S %Z%z')}.")
             await discord.utils.sleep_until(update)
