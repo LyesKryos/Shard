9fe2d870167420ca1969c66b47cd75ee001be808
LyesKryos <vizior.slosky@gmail.com>
2022-06-09 16:48:37 -0400

    Switching over to Discord.py v2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from ShardBot import Shard
 import asyncio
 
+
 async def main(bot: Shard):
     async with bot:
         await bot.start("ODM0ODkyMDM3MjE1NjE3MDk0.YIHfzQ.-UBhOq3ukC7kz3VTvUmpUBtjqaM")
 
-asyncio.run(main(Shard))
+
+asyncio.run(main(Shard()))
