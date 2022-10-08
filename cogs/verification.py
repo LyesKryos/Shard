d8fd9f91c291e17f087de9bfbda1f721ed932330
LyesKryos <vizior.slosky@gmail.com>
2022-10-08 07:55:12 -0400

    Fixing DM check

--- cogs/verification.py
+++ cogs/verification.py
@@ -24,26 +24,28 @@ class Verification(commands.Cog):
         conn = self.bot.pool
         # establishes author
         author = ctx.author
+        # creates DM
+        author_message = await author.create_dm()
 
-        def authorcheck(message):
-            return ctx.author == message.author and ctx.channel == discord.channel.DMChannel
+        def authorcheck():
+            return ctx.channel.id == author_message.id
 
         # sends DM to initiate verification
-        await author.send(f"**Welcome to the Thegye server, {author}!** \n\n"
-                          f"This is your quick invitation to verify your NationStates nation. If your nation is "
-                          f"currently residing in Thegye, you will be assigned the Thegye role. If your nation is"
-                          f" not currently residing in Thegye, you will be assigned the Traveler role. "
-                          f"If you do not verify any nation, you will be assigned the Unverified role and be unable"
-                          f" to access the majority of the server.\n\n"
-                          f"To begin the verification process, please enter your nation's **name**, "
-                          f"without the pretitle. For example, if your nation appears as `The Holy Empire of Bassiliya`,"
-                          f" please only enter `Bassiliya`. If you would like to skip verification, enter \"SKIP\" "
-                          f"instead of your nation name.")
+        await author_message.send(f"**Welcome to the Thegye server, {author}!** \n\n"
+                                  f"This is your quick invitation to verify your NationStates nation. If your nation is "
+                                  f"currently residing in Thegye, you will be assigned the Thegye role. If your nation is"
+                                  f" not currently residing in Thegye, you will be assigned the Traveler role. "
+                                  f"If you do not verify any nation, you will be assigned the Unverified role and be unable"
+                                  f" to access the majority of the server.\n\n"
+                                  f"To begin the verification process, please enter your nation's **name**, "
+                                  f"without the pretitle. For example, if your nation appears as `The Holy Empire of Bassiliya`,"
+                                  f" please only enter `Bassiliya`. If you would like to skip verification, enter \"SKIP\" "
+                                  f"instead of your nation name.")
         # waits for the user to reply with their nation
         try:
             nation_reply = await self.bot.wait_for('message', check=authorcheck, timeout=300)
         except asyncio.TimeoutError:
-            return await author.send("Verification timed out. Please answer me next time!")
+            return await author_message.send("Verification timed out. Please answer me next time!")
         # assigns nation name
         nation_name = nation_reply.content
         if nation_name.lower() == 'skip':
@@ -52,20 +54,21 @@ class Verification(commands.Cog):
         nation_exist = requests.get(f"https://www.nationstates.net/cgi-bin/api.cgi?nation={nation_name.lower()}")
         # if the nation does not exist, let the user know
         if nation_exist.status_code == 404:
-            await author.send(f"No such nation as `{nation_name}`. Please check that you are using only the nation's"
-                              f" name, without the pretitle.")
+            await author_message.send(
+                f"No such nation as `{nation_name}`. Please check that you are using only the nation's"
+                f" name, without the pretitle.")
             return
         # send verification instructions via DM
-        await author.send(f"Please login to {nation_name}. Once complete, head to this link and send the "
-                          f"verification code displayed: https://www.nationstates.net/page=verify_login. "
-                          f"You may give the code below to an external website, which can use it to verify that you are"
-                          f" indeed currently logged in as this nation. This is *all* I can do with it: "
-                          f"It does not allow me to access your nation.")
+        await author_message.send(f"Please login to {nation_name}. Once complete, head to this link and send the "
+                                  f"verification code displayed: https://www.nationstates.net/page=verify_login. "
+                                  f"You may give the code below to an external website, which can use it to verify that you are"
+                                  f" indeed currently logged in as this nation. This is *all* I can do with it: "
+                                  f"It does not allow me to access your nation.")
         # wait for response
         try:
             code_reply = await self.bot.wait_for('message', check=authorcheck, timeout=300)
         except asyncio.TimeoutError:
-            return await ctx.send("Verification timed out. Please answer me next time!")
+            return await author_message.send("Verification timed out. Please answer me next time!")
         # define headers and parameters
         headers = {"User-Agent": "Bassiliya"}
         params = {'a': 'verify',
@@ -96,15 +99,15 @@ class Verification(commands.Cog):
                         WHERE user_id = $1;''', author.id)
                         nation_check = [n.lower() for n in nation_check]
                         if nation_name in nation_check:
-                            await author.send("You have already verified that nation. To view a list of your verified"
-                                              " nations, use `$view_verified`.")
+                            await author_message.send("You have already verified that nation. "
+                                                      "To view a list of your verified nations, use `$view_verified`.")
                             return
                         # append the verified nation to the list
                         await conn.execute('''UPDATE verified_nations SET nations = nations || $1 
                         WHERE user_id = $2;''', nation_name, author.id)
-                    await author.send(f"Success! You have now verified `{nation_name}`. "
-                                      f"Your roles will update momentarily. If you would like to set your main nation, "
-                                      f"use the `$set_main` command to do so.")
+                    await author_message.send(f"Success! You have now verified `{nation_name}`. "
+                                              f"Your roles will update momentarily. If you would like to set your "
+                                              f"main nation, use the `$set_main` command to do so.")
                     verify.close()
                     # if the nation's region is Thegye, add the Thegye role
                     if verification_soup.region.text == "Thegye":
@@ -123,9 +126,8 @@ class Verification(commands.Cog):
                         await author.add_roles(traveler_role)
                     return
 
+
 async def setup(bot: Shard):
     # define the cog and add the cog
     cog = Verification(bot)
     await bot.add_cog(cog)
-
-
