ff9a5a177c24f4dc17cfedeb253c856f2a00bb74
LyesKryos <vizior.slosky@gmail.com>
2022-10-20 17:10:31 -0400

    Altering nation embed

--- cogs/ns.py
+++ cogs/ns.py
@@ -125,7 +125,7 @@ class NationStates(commands.Cog):
             nation_embed = discord.Embed(title="\u200b", colour=flag_color)
             nation_embed.set_thumbnail(url=flag_link)
             nation_embed.add_field(name=f"[{fullname}]"
-                                        f"(https://www.nationstates.net/nation={self.sanitize_links_underscore(name)}",
+                                        f"(https://www.nationstates.net/nation={self.sanitize_links_underscore(name)})",
                                    value="\u200b", inline=False)
             nation_embed.add_field(name="Motto", value=f"{motto}")
             nation_embed.add_field(name="Classification", value=f"{category}")
