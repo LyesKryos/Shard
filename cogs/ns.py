5e5ccd41bc016140ee26130094c7461877dfb726
LyesKryos <vizior.slosky@gmail.com>
2022-10-24 11:09:47 -0400

    Fixing region command

--- cogs/ns.py
+++ cogs/ns.py
@@ -203,15 +203,18 @@ class NationStates(commands.Cog):
             region_embed = discord.Embed(color=flag_color)
             region_embed.set_thumbnail(url=flag_link)
             region_embed.set_author(name=f"{region_name}", url=f"https://www.nationstates.net/region="
-                                                            f"{self.sanitize_links_underscore(region_name)}")
-            region_embed.add_field(name="Founder", value=f"[{founder}] (https://www.nationstates.net/nation="
-                                                         f"{self.sanitize_links_underscore(founder)}")
+                                                               f"{self.sanitize_links_underscore(region_name)}")
+            region_embed.add_field(name="Founder", value=f"[{founder.title()}] (https://www.nationstates.net/nation="
+                                                         f"{self.sanitize_links_underscore(founder)})")
             region_embed.add_field(name="Founded",
                                    value=f"{creation_time.strftime('%d %b %Y')}\n"
                                          f"({founded})")
             region_embed.add_field(name="\u200b", value="\u200b")
             region_embed.add_field(name='Nations', value=f"{residents} nations")
-            region_embed.add_field(name="World Assembly Delegate", value=f"{delegate}\n({len(del_endos)} votes)")
+            region_embed.add_field(name="World Assembly Delegate", value=f"[{delegate.title()}] "
+                                                                         f"(https://www.nationstates.net/nation="
+                                                                         f"{self.sanitize_links_underscore(delegate)})"
+                                                                         f"\n({len(del_endos)} votes)")
             region_embed.add_field(name="Influence", value=f"{influence_level}")
             region_embed.add_field(name="Last Update", value=f"<t:{update}:T>")
             await ctx.send(embed=region_embed)
