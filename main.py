7b607277d02ffa622e709590a36f998939a645b1
LyesKryos <vizior.slosky@gmail.com>
2022-11-07 17:32:10 -0500

    Fixing startup

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from ShardBot import Shard
 import asyncio
 
 
-async def main(bot: Shard):
-    async with bot:
+def main(bot: Shard):
+    with bot:
         bot.run("ODM0ODkyMDM3MjE1NjE3MDk0.YIHfzQ.-UBhOq3ukC7kz3VTvUmpUBtjqaM", log_handler=bot.logger)
 
 
-asyncio.run(main(Shard()))
+main(Shard())
