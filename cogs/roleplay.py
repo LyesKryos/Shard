42be47e0d886d4f864978ae5474a926fa2ebf458
LyesKryos <vizior.slosky@gmail.com>
2023-02-10 08:15:46 -0500

    Fixing RSC checking

--- cogs/roleplay.py
+++ cogs/roleplay.py
@@ -18,8 +18,6 @@ class Roleplay(commands.Cog):
                       brief="Calculates total points and percentages for a given dispatch")
     @commands.guild_only()
     async def rsc_check(self, ctx, dispatch_link):
-        # fetch dispatch id, establish headers and params, and make API call
-        dispatch_id = (re.findall('\d+', dispatch_link))[0]
         while True:
             # see if there are enough available calls. if so, break the loop
             try:
@@ -29,31 +27,28 @@ class Roleplay(commands.Cog):
             except TooManyRequests as error:
                 await asyncio.sleep(int(str(error)))
                 continue
+        # fetch dispatch id, establish headers and params, and make API call
+        dispatch_id = (re.findall('\d+', dispatch_link))[0]
         headers = {'User-Agent': 'Bassiliya'}
         params = {'q': 'dispatch',
                   'dispatchid': f"{dispatch_id}"}
         response = requests.get('https://www.nationstates.net/cgi-bin/api.cgi', headers=headers, params=params)
-        # if the call is rejected or otherwise causes an issue
         if response.status_code != 200:
             return IndexError
-        # parse the html and pull out all numbers followed by /100
         textsoup = BeautifulSoup(response.text, 'html.parser')
-        dtext = textsoup.find('TEXT').string
+        dtext = textsoup.find('text').string
         numbers = re.findall(r'\d+?/100', dtext)
         total = 0
-        # add all numbers together
         for n in numbers:
             value = re.sub('/100', '', n)
             total += int(value)
-        # find and add all percents together
         percents = re.findall(r'\d+?&#37', dtext)
         percents_total = 0
         for p in percents:
             value = re.sub('&#37', '', p)
             percents_total += int(value)
-        # send totals
-        await ctx.send(f"Total: {total} points\nTotal Percentages: {percents_total} percentage points")
-        return
+        return await ctx.send(f"Total: {total} points\nTotal Percentages: {percents_total} percentage points")
+
 
     @commands.command(usage="[user]", aliases=['rpi'], brief="Gives information to a user about roleplay.")
     @commands.has_role(674278988323225632)
