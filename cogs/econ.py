581cbafc05afc768320e41b36eede8c7757c4a0b
LyesKryos <vizior.slosky@gmail.com>
2023-03-17 11:50:45 -0400

    Fixing view for info command

--- cogs/econ.py
+++ cogs/econ.py
@@ -940,7 +940,7 @@ class Economy(commands.Cog):
             if loan is not None:
                 rbtm_embed.add_field(name="Loan Account", value=f"{self.thaler}{loan['amount']:,.2f}")
                 total_value -= loan['amount']
-            rbtm_embed.add_field(name="Net Worth", value=f"{self.thaler}{total_value:,.2f}")
+            rbtm_embed.add_field(name="Net Worth", value=f"{self.thaler}{total_value:,.2f}", inline=False)
             return await interaction.followup.send(embed=rbtm_embed)
 
     @rbt.command(name='directory', description="Displays all registered members of the Royal Bank of Thegye.")
