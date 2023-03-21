2514c2350a53bd9fad3299f488bcf986eb98bf7d
LyesKryos <vizior.slosky@gmail.com>
2023-03-21 13:39:17 -0400

    Adding wallet size

--- cogs/econ.py
+++ cogs/econ.py
@@ -1874,6 +1874,12 @@ class Economy(commands.Cog):
             if tax < .01:
                 tax = .01
             transaction = round(base_price + tax, 2)
+        # check the user's wallet
+        wallet_contents = await conn.fetchrow('''SELECT sum(amount) FROM ledger WHERE user_id = $1;''', user.id)
+        if wallet_contents['sum'] > rbt_member['wallet']:
+            return await interaction.followup.send(f"Your wallet (size: {rbt_member['wallet']}) "
+                                                   f"does not have enough room to buy {amount} "
+                                                   f"shares of {stock['name']}.")
         # if the stock would become overdrawn, notify user
         if amount + stock['outstanding'] > stock['issued']:
             return await interaction.followup.send(f"Purchasing {amount} shares of {stock['name']} would cause it to "
