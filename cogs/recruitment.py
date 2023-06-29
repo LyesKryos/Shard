b49c404dd174c1d8bd8934f4034aa6d3d1c66c04
LyesKryos <vizior.slosky@gmail.com>
2023-06-29 11:36:19 -0400

    Fixing autogrammer auto-waiting

--- cogs/recruitment.py
+++ cogs/recruitment.py
@@ -396,7 +396,7 @@ class Recruitment(commands.Cog):
                     async with session.get('https://www.nationstates.net/cgi-bin/api.cgi?',
                                            headers=headers, params=telegram_params) as tg_response:
                         if tg_response.status == 429:
-                            retry = tg_response.headers['X-Retry-After']
+                            retry = tg_response.headers['Retry-After']
                             await asyncio.sleep(int(retry))
                             await crashchannel.send(f"Too many requests. Retrying after {int(retry)} seconds.")
                         elif tg_response.status != 200:
@@ -639,13 +639,8 @@ class Recruitment(commands.Cog):
     @commands.is_owner()
     async def cut_autogrammer(self, ctx):
         # stops autogrammer
-        message = await ctx.send("Cutting...")
+        await ctx.send("Cutting...")
         self.autogrammer_task.cancel()
-        await asyncio.sleep(1)
-        if self.autogrammer_task.cancelled():
-            await message.edit(content="Autogrammer cut.")
-        else:
-            await message.edit(content="Autogrammer uncut.")
 
     @commands.command()
     @commands.is_owner()
