60b9aaa23d3fa3f507501c93a30992deaf0551c9
LyesKryos <vizior.slosky@gmail.com>
2023-06-29 10:58:33 -0400

    Fixing autogrammer

--- cogs/recruitment.py
+++ cogs/recruitment.py
@@ -384,7 +384,7 @@ class Recruitment(commands.Cog):
         self.world_assembly_notification.cancel()
 
     def cog_load(self) -> None:
-        asyncio.get_running_loop().run_until_complete(self.autogrammer())
+        asyncio.run(self.autogrammer())
 
     # cog variables
     do_not_recruit = list()
