b047ada41b8d9d7b807abea6a504041283a24df4
LyesKryos <vizior.slosky@gmail.com>
2023-06-30 16:30:36 -0400

    Fixing welcome messsage for Senators

--- cogs/roleplay.py
+++ cogs/roleplay.py
@@ -119,18 +119,18 @@ class Roleplay(commands.Cog):
                                f"Wiki page: {senator_wiki_page}")
         # post information in #smoking_room
         smoking_room = self.bot.get_channel(1106961957002686464)
-        await smoking_room.send(f"Senators, please welcome {interaction.user.mention} to the Grand Senate of the "
-                                f"United Kingdom of Thegye!\n\n"
+        await smoking_room.send(f"Senators, please welcome {senator_name} ({interaction.user.mention})"
+                                f" to the Grand Senate of the United Kingdom of Thegye!\n\n"
                                 f"Now that your application is approved, you can begin being involved by saying hello"
-                                f"in the https://discord.com/channels/674259612580446230/1112080185949437983"
+                                f"in the <https://discord.com/channels/674259612580446230/1112080185949437983>"
                                 f" OOC chat or introducing your character in the "
-                                f"https://discord.com/channels/674259612580446230/1106961957002686464 IC chat."
+                                f"<https://discord.com/channels/674259612580446230/1106961957002686464> IC chat."
                                 f"You can also check out current legislation on the #floor, "
                                 f"proposals in #the_chancellery, and the most recent news in the #newsroom. You can"
                                 f"also create an account on our dedicated wiki (<https://thegye.miraheze.org/>),"
                                 f"where legislation, characters, Ministry of Information Reports, and more are stored"
                                 f"and created. If you have questions, feel free to ask them in "
-                                f"https://discord.com/channels/674259612580446230/1112080185949437983. "
+                                f"<https://discord.com/channels/674259612580446230/1112080185949437983>. "
                                 f"Once again, welcome!")
         await interaction.user.add_roles(senator_role)
         await interaction.followup.send("Application submitted!")
