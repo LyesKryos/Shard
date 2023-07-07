65ddaa4a8df11793330568d9f2a5b6e7d91d7115
LyesKryos <vizior.slosky@gmail.com>
2023-07-07 15:26:46 -0400

    Troubleshooting svg

--- cogs/ns.py
+++ cogs/ns.py
@@ -121,8 +121,8 @@ class NationStates(commands.Cog):
             try:
                 img = Image.open(BytesIO(get_flag))
             except PIL.UnidentifiedImageError:
-                img = svg2png(file_obj=get_flag)
-                await ctx.send(img)
+                img = svg2png(url=flag_link)
+
             rgb_color = self.get_dominant_color(img)
             flag_color = discord.Colour.from_rgb(rgb_color[0], rgb_color[1], rgb_color[2])
             creation_time = datetime.datetime.fromtimestamp(int(founded_epoch), tz=self.eastern)
