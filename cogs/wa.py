1e344f084f17e2259f32b1988744e409ba0e0d79
LyesKryos <vizior.slosky@gmail.com>
2023-07-30 15:49:32 -0400

    Diagnosis of dump

--- cogs/wa.py
+++ cogs/wa.py
@@ -128,10 +128,10 @@ class WA(commands.Cog):
                         else:
                             items.append(region.text.strip())
                     if region.tag == "FOUNDERAUTH":
-                        if region.text is not None:
-                            items.append(region.text.strip())
-                        else:
+                        if region.text is None:
                             items.append("Non-exec")
+                        else:
+                            items.append(region.text.strip())
                     if region.tag == "POWER":
                         items.append(region.text.strip())
                     if region.tag == "NATIONS":
