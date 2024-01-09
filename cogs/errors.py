e4179481882f9988e3422ce902ad71e9c2d751f7
LyesKryos <vizior.slosky@gmail.com>
2024-01-09 14:41:20 -0500

    Testing new error handling

--- cogs/errors.py
+++ cogs/errors.py
@@ -82,8 +82,7 @@ class ShardErrorHandler(commands.Cog):
                                                     ephemeral=True)
         elif isinstance(error, app_commands.errors.MissingRole):
             await interaction.response.send_message("You are missing the proper roles for this command.")
-        else:
-            await interaction.channel.send("An error occurred, check the logs.")
+
 
 async def setup(bot: Shard):
     errorhandler = ShardErrorHandler(bot)
