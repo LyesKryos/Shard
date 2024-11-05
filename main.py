8b8cfab7fcb860bfb531b3119bc8e1e2e4dc20fd
Lyes Kryos <82159357+LyesKryos@users.noreply.github.com>
2024-11-04 19:31:25 -0500

    Trying to fix error logging

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging.handlers as handlers
 
 def main(bot: Shard):
     handler = handlers.RotatingFileHandler("botlogs.log", encoding="utf-8",
-                                           mode='a', maxBytes=100000, backupCount=3)
+                                           mode='a', maxBytes=1000000)
     handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s: %(message)s"))
     handler.setLevel(logging.INFO)
     bot.run("ODM0ODkyMDM3MjE1NjE3MDk0.YIHfzQ.-UBhOq3ukC7kz3VTvUmpUBtjqaM", log_handler=handler)
