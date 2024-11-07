fbd0c091933c5247e7980669c5e1bc610a6d27b7
Lyes Kryos <82159357+LyesKryos@users.noreply.github.com>
2024-11-06 19:15:01 -0500

    Editing add party command.

--- cogs/roleplay.py
+++ cogs/roleplay.py
@@ -192,7 +192,10 @@ class Roleplay(commands.Cog):
         conn = self.bot.pool
         # create party role
         thegye_server = self.bot.get_guild(674259612580446230)
+        senator = thegye_server.get_role(1109211491170783293)
+        position = senator.position
         new_party_role = await thegye_server.create_role(name=party_name, color=discord.Color.from_str(party_color))
+        await new_party_role.edit(position=position-1)
         # assign the role to the leader
         party_leader_role = thegye_server.get_role(1124422828641505300)
         await leader.add_roles(party_leader_role, new_party_role)
