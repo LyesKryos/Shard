5aabc284bc5cbb917167e490ef28fc710b2b517c
Lyes Kryos <82159357+LyesKryos@users.noreply.github.com>
2024-11-11 18:30:36 -0500

    Trying to fix tech tree.

--- cogs/cnc.py
+++ cogs/cnc.py
@@ -708,7 +708,9 @@ class CNC(commands.Cog):
             await interaction.followup.send(gear_cords)
             tech_map.paste(gear_icon, (int(gear_cords[0]/3.778), int(gear_cords[1]/3.778)) , mask=gear_icon)
         # save image
-        tech_map.save(fr"{self.tech_directory}CNC Tech Map Rendered.png", optimize=True, quality=53)
+        size = tech_map.size
+        tech_map.resize(size[0]//2, size[1]//2, resample=Image.LANCZOS)
+        tech_map.save(fr"{self.tech_directory}CNC Tech Map Rendered.png")
         # upload image
         await interaction.followup.send(file=discord.File(fr"{self.tech_directory}CNC Tech Map Rendered.png"))
 
