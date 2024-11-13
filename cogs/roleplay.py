4d2e0534fba49725c69926089648a3d9271e3fe3
Lyes Kryos <82159357+LyesKryos@users.noreply.github.com>
2024-11-13 17:36:41 -0500

    Updating the count_members command

--- cogs/roleplay.py
+++ cogs/roleplay.py
@@ -416,10 +416,10 @@ class Roleplay(commands.Cog):
         message = "**Senators**: 540\n"
         senators_in_parties = 0
         for p in party_roles:
-            message += f"{p.name}: {math.floor((len(p.members)/senator_number)*540)}\n"
+            message += f"{p.name.capitalize()}: {math.floor((len(p.members)/senator_number)*540)}\n"
             senators_in_parties += math.floor((len(p.members)/senator_number)*540)
         message += f"Independents: {540-senators_in_parties}\n"
-        message += "\n\n*Note: these numbers are provisional and are not official.*"
+        message += "\n*Note: these numbers are provisional and are not official.*"
         # send message
         return await interaction.followup.send(message)
 
