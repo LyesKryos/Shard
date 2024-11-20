6cc0435817f72a5a5c6f44aa5051c04312abd797
Lyes Kryos <82159357+LyesKryos@users.noreply.github.com>
2024-11-19 19:12:03 -0500

    Making the nation command visually interesting

--- cogs/cnc.py
+++ cogs/cnc.py
@@ -497,13 +497,13 @@ class CNC(commands.Cog):
         # populate stability
         user_embed.add_field(name="Stability", value=f"{user_info['stability']}")
         # populate all three types of authority
-        user_embed.add_field(name="====================AUTHORITY====================",
+        user_embed.add_field(name="===================AUTHORITY======================",
                              value="Information known about the nation's authority.", inline=False)
         user_embed.add_field(name="Political Authority", value=f"{user_info['pol_auth']}")
         user_embed.add_field(name="Military Authority", value=f"{user_info['mil_auth']}")
         user_embed.add_field(name="Economic Authority", value=f"{user_info['econ_auth']}")
         # populate all four types of relations
-        user_embed.add_field(name="====================RELATIONS====================",
+        user_embed.add_field(name="=====================RELATIONS====================",
                              value="Information known about the nation's international relationships.", inline=False)
         user_embed.add_field(name="Allies", value=f"{allies}")
         user_embed.add_field(name="Wars", value=f"{wars}")
