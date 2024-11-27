b7ac5a435f18314d5b9ec9784b5553aa3f457f00
Lyes Kryos <82159357+LyesKryos@users.noreply.github.com>
2024-11-27 12:44:23 -0500

    Fixing development boosting command

--- cogs/cnc.py
+++ cogs/cnc.py
@@ -1287,7 +1287,7 @@ class CNC(commands.Cog):
                     f"province. You are missing {boost_cost - user_info['mil_auth']} "
                     f"Political authority.")
         # execute orders
-        await conn.execute('''UPDATE cnc_users SET "$1" = "$1" - $2 WHERE user_id = $3;''', type, boost_cost,
+        await conn.execute('''UPDATE cnc_users SET col($1) = col($1) - $2 WHERE user_id = $3;''', type, boost_cost,
                            interaction.user.id)
         await conn.execute('''UPDATE cnc_provinces SET development = development + 1 WHERE id = $2;''', province_id)
         return await interaction.followup.send(f"Successfully boosted Development! The total development of the province"
