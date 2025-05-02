5d5af6c6f4a227056cb39ddb928d4ec6c95b9bae
Lyes Kryos <82159357+LyesKryos@users.noreply.github.com>
2025-05-02 19:11:43 -0400

    Updating portfolio long

--- cogs/econ.py
+++ cogs/econ.py
@@ -275,7 +275,6 @@ class Pageinate(View):
             return await self.interaction.edit_original_response(view=self)
         else:
             self.page -= 1
-            back_button.disabled = False
             ledger_string = await portfolio(self.ledger_info, self.bot.pool, "\u20B8", self.page)
             port_embed = self.embed.set_field_at(index=-1, name="Stocks and Shares", value=ledger_string)
             return await self.interaction.edit_original_response(view=self, embed=port_embed)
@@ -293,7 +292,6 @@ class Pageinate(View):
     async def forward(self, interaction: discord.Interaction, forward_button: discord.Button):
         # defer
         await interaction.response.defer()
-        forward_button.disabled = False
         if self.page >= self.max_page:
             forward_button.disabled = True
             # edit view
@@ -304,6 +302,7 @@ class Pageinate(View):
             port_embed = self.embed.set_field_at(index=-1, name="Stocks and Shares", value=ledger_string)
             if self.page >= self.max_page:
                 forward_button.disabled = True
+            self.back.disabled = False
             return await self.interaction.edit_original_response(view=self, embed=port_embed)
 
 
