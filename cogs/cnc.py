640df53a0814ab4e8ebae80ce6052d2bfbc7b248
Lyes Kryos <82159357+LyesKryos@users.noreply.github.com>
2025-05-10 17:09:37 -0400

    Adding authority to government embed

--- cogs/cnc.py
+++ cogs/cnc.py
@@ -2243,11 +2243,12 @@ class CNC(commands.Cog):
         govt_embed.add_field(name="Stability", value=f"{user_info['stability']}")
         govt_embed.add_field(name="Overextension Limit", value=f"{user_info['overextend_limit']}")
         # populate current authority levels
-        govt_embed.add_field(name="Economic Authority            Military Authority            Political Authority",
-                             value=f"{user_info['econ_auth']}\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020"
-                                   f"{user_info['mil_auth']}\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020"
-                                   f"{user_info['pol_auth']}",
-                             inline=False)
+        govt_embed.add_field(name="Economic Authority",
+                             value=f"{user_info['econ_auth']}")
+        govt_embed.add_field(name="Military Authority",
+                             value=user_info['mil_auth'])
+        govt_embed.add_field(name="Political Authority",
+                             value=user_info['pol_auth'])
         # populate base authority gains
         govt_embed.add_field(name="Base Economic Authority Gain", value=govt_info['econ_auth'])
         govt_embed.add_field(name="Base Military Authority Gain", value=govt_info['mil_auth'])
