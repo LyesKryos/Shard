6d8476ac5450ada95a91c8ea02c806e9423658f1
Lyes Kryos <82159357+LyesKryos@users.noreply.github.com>
2025-05-19 13:13:14 -0400

    Fixing government reform bug

--- cogs/cnc.py
+++ cogs/cnc.py
@@ -1587,11 +1587,9 @@ class GovernmentReformTypeEnact(discord.ui.View):
         unrest = unrest + 10
         WHERE user_id = $5;''', subtype['pretitle'], govt_info['govt_type'], subtype['govt_subtype'],
                            subtype['manpower'], self.interaction.user.id)
-        # testing
-        embed = interaction.message.embeds[0]
-        await interaction.followup.send(embed)
+
         # edit embed
-        govt_embed = self.govt_embed
+        govt_embed = interaction.message.embeds[0]
         # update authority gains
         govt_embed.set_field_at(7, name="Base Economic Authority Gain", value=subtype['econ_auth'])
         govt_embed.set_field_at(8, name="Base Military Authority Gain", value=subtype['mil_auth'])
