1d8e2035f26229019c912fd595f8d2ed6b2572d7
Lyes Kryos <82159357+LyesKryos@users.noreply.github.com>
2025-10-29 13:12:38 -0400

    Load bot token from config.json
    Added functionality to read the bot token from a config file.

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from ShardBot import Shard
 import logging
 import logging.handlers as handlers
 import asyncio
+import json
 
 def main(bot: Shard):
+    # read config file
+    token_raw = json.load(open("config.json"))
+    token = token_raw["token"]
     # setup logger
     handler = handlers.RotatingFileHandler("botlogs.log", encoding="utf-8",
                                            mode='a', maxBytes=50000, backupCount=3)
     handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s: %(message)s"))
     handler.setLevel(logging.INFO)
-    bot.run(log_handler=handler)
+    bot.run(log_handler=handler, token=token)
 
 main(Shard())
