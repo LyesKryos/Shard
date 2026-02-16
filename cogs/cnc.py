7e2478487464c6f5c43fafc9fde5ba2597b26e6e
Lyes Kryos <82159357+LyesKryos@users.noreply.github.com>
2026-02-16 12:55:06 -0500

    Correcting unnecesary double dipping for buffer_output creation at 1278.

--- cogs/cnc.py
+++ cogs/cnc.py
@@ -1276,7 +1276,6 @@ class DossierView(View):
                 # for each relation, join to a comma-separated list if the relation "member" isn't the user's nation
                 for relation in relations:
                     buffer_output = ", ".join([r for r in relation['attackers'] if r != name])
-                    buffer_output += ", ".join([r for r in relation['defenders'] if r != name])
                     output += buffer_output
                 return output
             else:
