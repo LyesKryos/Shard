3235bba9453ced144c3ae3c753c3868c1f82b8fb
Lyes Kryos <82159357+LyesKryos@users.noreply.github.com>
2026-02-26 14:03:45 -0500

    Trying to correct interaction handling

--- cogs/cnc.py
+++ cogs/cnc.py
@@ -4436,6 +4436,10 @@ class WarOptionsView(discord.ui.View):
 
                         async def callback(self, interaction: discord.Interaction):
                             self.view.mil_authority = self.values[0]
+                                try:
+                                    await interaction.response.defer(thinking=False)  # or thinking=False if you want silent
+                                except discord.InteractionResponded:
+                                    pass  # ignore if already responded somehow
 
                     class EconSelect(discord.ui.Select):
                         def __init__(self):
@@ -4448,6 +4452,10 @@ class WarOptionsView(discord.ui.View):
 
                         async def callback(self, interaction: discord.Interaction):
                             self.view.econ_authority = self.values[0]
+                            try:
+                                await interaction.response.defer(thinking=False)  # or thinking=False if you want silent
+                            except discord.InteractionResponded:
+                                pass  # ignore if already responded somehow
 
                     class DiploSelect(discord.ui.Select):
                         def __init__(self):
@@ -4460,6 +4468,10 @@ class WarOptionsView(discord.ui.View):
 
                         async def callback(self, interaction: discord.Interaction):
                             self.view.diplo_authority = self.values[0]
+                            try:
+                                await interaction.response.defer(thinking=False)  # or thinking=False if you want silent
+                            except discord.InteractionResponded:
+                                pass  # ignore if already responded somehow
 
                     # create submit and cancel buttons
 
