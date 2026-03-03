b327b40f8e4f493bfdce832522a9a9c774736141
Lyes Kryos <82159357+LyesKryos@users.noreply.github.com>
2026-03-03 15:23:43 -0500

    Adding a pathfinder testing function

--- cogs/cnc.py
+++ cogs/cnc.py
@@ -12,6 +12,8 @@ from base64 import b64encode
 import requests
 from discord.ui import View
 import math
+import time
+import heapq
 
 
 async def safe_dm(bot: discord.Client, user_id: int, *, content: str | None = None,
@@ -7645,6 +7647,97 @@ class CNC(commands.Cog):
             await conn.execute('''UPDATE cnc_provinces
                                   SET citizens = development * (RANDOM() * (1500 - 500) + 500);''')
         return await ctx.send("World populated.")
+
+    @commands.command()
+    @commands.is_owner()
+    async def cnc_pathfinder_test(self, ctx, origin: int, destination: int):
+        # establish connection
+        conn = self.bot.pool
+        # define start time
+        start_time = time.perf_counter()
+        # define the province map
+        p_map = {}
+        # define the cost map
+        pc_map = {}
+        # pull province information and put it into a dict
+        all_provinces = await conn.fetch('''SELECT id, river, structures, bordering FROM cnc_provinces;''')
+        for p in all_provinces:
+            p_map[p['id']] = p['bordering']
+            pc_map[p['id']] = 1
+            # if there is a road, the cost is halved (unles there is a river)
+            if "Road" in p['structures']:
+                pc_map[p['id']] *= 0.5
+            # if there is a river and not a bridge, the cost is 2
+            if (p['river']) and ("Bridge" not in p['structures']):
+                pc_map[p['id']] = 2
+
+        # now define the Dijkstra's algorithm
+        def find_path(start_id, end_id):
+            """This program utilizes Dijkstra's algorithm to find the shortest path between two provinces."""
+
+            # define the best cost
+            best_cost = {start_id: 0}
+            # define the previous so we know which way to go
+            previous = {}
+            # establish the queue as a tuple because of heapq sorting
+            queue = [(0,start_id)]
+            
+            # while there are items in the queue
+            while queue:
+                
+                # get the lowest current value
+                cost, tile = heapq.heappop(queue)
+
+                # if the cost is greater than the best cost, then we skip it because it is stale
+                # remember, infinity = unknown distance
+                # this means that if the tile is unknown, the cost is infinite 
+                if cost > best_cost.get(tile, float('inf')):
+                    continue
+                
+                # if we have finally reached our sestination, break
+                if tile == end_id:
+                    break
+
+                # now we iterate for each neighbor in the map
+                for neighbor in p_map[tile]:
+                    # add the cost of moving to this tile from the current tile
+                    new_cost = cost + pc_map[neighbor]
+                    # if the new cost is better than the current cost (or an unknown cost)
+                    if new_cost < best_cost.get(neighbor, float('inf')):
+                        # the best cost for that neighbor becomes the new cost, since it is less than any of the ones we know
+                        best_cost[neighbor] = new_cost
+                        # then we add the current tile to the previous so we can reconstruct
+                        previous[neighbor] = tile
+                        # then push this tile into the queue
+                        heapq.heappush(queue, (new_cost, neighbor))
+
+            # if the end id is not anywhere in the best cost
+            if end_id not in best_cost:
+                return None
+
+            # if we did reach it, reconstruct the path
+            path = []
+            tile = end_id
+            # then cycle through the tiles to reconstruct the path
+            while tile is not None:
+                # append the current path tile
+                path.append(tile)
+                # then get the next tile in the previous path dict
+                tile = previous.get(tile)
+            # once we have made it to the end of the previous dict, we will reverse the order of the pathway 
+            path.reverse()
+            # return the path and the cost
+            return path, best_cost[end_id]
+        
+        # run the function
+        path, cost = find_path(origin, destination)
+        # if there is no path, return such
+        if path is None:
+            return await ctx.send(f"Province {destination} cannot be reached by land from Province {origin}.")
+        # otherwise, return and send
+        return await ctx.send(f"The total cost of travel is {cost} points.\nThe path there travels through the following province IDs: {path}}")
+
+
 
 
 async def setup(bot: Shard):
