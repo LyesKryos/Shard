b1caaea44572475d22d5ec808521d62df1b54325
Lyes Kryos <82159357+LyesKryos@users.noreply.github.com>
2026-04-14 12:58:32 -0400

    Updating dossier to include more economic information

--- cogs/cnc.py
+++ cogs/cnc.py
@@ -1848,7 +1848,7 @@ class DossierView(View):
         trade_goods_produced = await self.conn.fetchval('''SELECT array_agg(trade_good) FROM cnc_provinces WHERE owner_id = $1;''', self.user_info['user_id'])
         self.doss_embed.add_field(name="Production Last Turn", value=f"{total_production} production")
         self.doss_embed.add_field(name="Average Development", value=f"{average_development:.2f} development")
-        self.doss_embed.add_field(name="Trade Goods Produced", value=f"{', '.join(trade_goods_produced)}")
+        self.doss_embed.add_field(name="Trade Goods Produced", value=f"{', '.join(trade_goods_produced)}", inline=False)
         # update
         await interaction.edit_original_response(embed=self.doss_embed)
 
